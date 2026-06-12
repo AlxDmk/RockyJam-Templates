939e6582693a10d13b68c5aba50d22f50e9ae9ad
AlxDmk <87035808+AlxDmk@users.noreply.github.com>
2026-06-12 11:26:30 +0300

    revert test: restore Add to Cart hook for rogue-product; let Hook Manager control it via hooks-config

--- templates/rogue-product/hooks.php
+++ templates/rogue-product/hooks.php
@@ -72,9 +72,9 @@ if ( function_exists( 'rockyjam_product_badges_render' ) ) {
 	add_action( 'woocommerce_single_product_summary', 'rockyjam_product_badges_render', 45 );
 }
 
-// TEST: completely disable the default Add to Cart button for rogue-product
+// Restore standard Add to Cart button for rogue-product
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 50 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 50 );
 
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 60 );
